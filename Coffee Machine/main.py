dac9cced08076904a46963359b6c2e4ff9d8cce3
Mithra Perera <mithraperera@hotmail.com>
2023-01-22 02:28:38 -0500

    coffee machine completed

---
+++ Coffee Machine/main.py
@@ -0,0 +1,89 @@
+from art import logo
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "milk": 0,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+def collect_coins(type_coffee):
+    print("Please insert coins.")
+    quarters = int(input("How many quarters?: "))
+    dimes = int(input("How many dimes?: "))
+    nickles = int(input("How many nickles?: "))
+    pennies = int(input("How many pennies?: "))
+    total_inputted = (0.25 * quarters) + (0.10 * dimes) + (0.05 * nickles) + (0.01 * pennies)
+    cost = MENU[type_coffee]["cost"]
+    cost = MENU[type_coffee]["cost"]
+    cost = MENU[type_coffee]["cost"]
+
+    if total_inputted >= cost:
+        change = total_inputted - cost
+        print("💵: Here is ${:.2f} in change.".format(change))
+        print(f"☕️: Here is your {type_coffee}. Enjoy!")
+        return True
+    else:
+        return False
+
+
+def check_ingredients(type_coffee):
+    if (MENU[type_coffee]["ingredients"]["water"] <= resources["water"]) and (
+            MENU[type_coffee]["ingredients"]["milk"] <= resources["milk"]) and (
+            MENU[type_coffee]["ingredients"]["coffee"] <= resources["coffee"]):
+        return True
+    else:
+        if MENU[type_coffee]["ingredients"]["water"] > resources["water"]:
+            print("Sorry, there is not enough water.")
+        if MENU[type_coffee]["ingredients"]["milk"] > resources["milk"]:
+            print("Sorry, there is not enough milk.")
+        if MENU[type_coffee]["ingredients"]["coffee"] > resources["coffee"]:
+            print("Sorry, there is not enough coffee.")
+        return False
+
+
+def use_resources(type_coffee):
+    resources["water"] -= MENU[type_coffee]["ingredients"]["water"]
+    resources["milk"] -= MENU[type_coffee]["ingredients"]["milk"]
+    resources["coffee"] -= MENU[type_coffee]["ingredients"]["coffee"]
+
+
+def coffee_machine():
+    while True:
+        type = input("What would you like? ('e' - espresso/'l' - latte/'c' - cappuccino): ")
+        enough_ingredients = check_ingredients(type)
+        if enough_ingredients:
+            successful_purchase = collect_coins(type)
+            use_resources(type)
+
+
+print(logo)
+print("PRICES: Espresso = $1.50, Latte = $2.50, Cappuccino = $3.00")
+coffee_machine()
